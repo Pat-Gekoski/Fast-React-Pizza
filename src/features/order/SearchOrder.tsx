17bccc08857aa90626795b4cd9caa5c671b27a78
Patric Gekoski <=>
2024-09-10 15:48:08 -0400

    Finish section 22

---
+++ src/features/order/SearchOrder.tsx
@@ -0,0 +1,23 @@
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SearchOrder() {
+	const [query, setQuery] = useState('');
+	const navigate = useNavigate();
+
+	function handleSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		if (!query) return;
+
+		navigate(`/order/${query}`);
+		setQuery('');
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input placeholder='Search Order #' value={query} onChange={(e) => setQuery(e.target.value)} />
+		</form>
+	);
+}
+
+export default SearchOrder;
